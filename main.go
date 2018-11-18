000acc7a00dad9d21edcc17c671ee2dc59ccd675
1Franck <snake386@hotmail.com>
2018-11-17 21:49:43 -0500

    added protocol

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func showResponse(response *http.Response) {
 		fmt.Printf("\nRedirected to ...\n")
 	}
 	fmt.Printf("[#%v] %v - %v", nbOfJumps, response.Request.URL.String(), redirectTimes[nbOfJumps-1])
-	fmt.Printf("\n > Status: %v\n", response.Status)
+	fmt.Printf("\n > Status: %v\n > Protocol: %v\n", response.Status, response.Proto)
 	for i, v := range response.Header {
 		if i != "Location" {
 			fmt.Printf(" > %v: %v\n", i, v)
